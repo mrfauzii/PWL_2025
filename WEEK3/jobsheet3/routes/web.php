48d5d574afe5a33c3d7cd7d27d5cab5f50b0f27d
mrfauzii <reishifauzi24@gmail.com>
2025-03-12 02:27:40 +0700

    update eloquent orm

--- WEEK3/jobsheet3/routes/web.php
+++ WEEK3/jobsheet3/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
