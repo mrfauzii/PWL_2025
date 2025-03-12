0593302654b68f704c068d941e0fdc7e1cfadc30
mrfauzii <reishifauzi24@gmail.com>
2025-03-12 11:11:52 +0700

    update retrieving single models

--- WEEK4/jobsheet4/app/Http/Controllers/UserController.php
+++ WEEK4/jobsheet4/app/Http/Controllers/UserController.php
@@ -10,13 +10,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
         
         // tambah data user dengan Eloquent Model
         // $data = [
@@ -30,12 +30,17 @@ class UserController extends Controller
         // $data = [
         //     'nama' => 'Pelanggan Pertama',
         // ];
-        
+
         // UserModel::where('username', 'customer-1')->update($data); // update data user
         
 
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
