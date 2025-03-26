67f5fe1ed73045ddc080f426d186de16c1287aa0
mrfauzii <reishifauzi24@gmail.com>
2025-03-26 21:05:20 +0700

    update penerapan layouting

--- WEEK5.2/jobsheet5/resources/views/layouts/template.blade.php
+++ WEEK5.2/jobsheet5/resources/views/layouts/template.blade.php
@@ -23,8 +23,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ assets(adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img">
+      <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
     <!-- Sidebar -->
@@ -39,13 +39,13 @@
 
     <!-- Main content -->
     <section class="content">
-    @yield('content')
+        @yield('content')
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
-    @include('laylouts.footer')
+    @include('layouts.footer')
 </div>
 <!-- ./wrapper -->
 
--- WEEK5.2/jobsheet5/routes/web.php
+++ WEEK5.2/jobsheet5/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
@@ -10,6 +11,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', [WelcomeController::class, 'index']);
+
+
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
@@ -18,3 +22,9 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/cek-footer', function () {
+    dd(file_exists(resource_path('views/layouts/footer.blade.php')));
+});
+
+
