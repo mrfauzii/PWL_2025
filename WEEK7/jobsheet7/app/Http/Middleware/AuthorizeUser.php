bc8203e8087eee4d782e749cc10a5da73cd629e2
mrfauzii <reishifauzi24@gmail.com>
2025-04-16 02:00:05 +0700

    update prak3 implementasi multi level authorization

--- WEEK7/jobsheet7/app/Http/Middleware/AuthorizeUser.php
+++ WEEK7/jobsheet7/app/Http/Middleware/AuthorizeUser.php
@@ -11,17 +11,17 @@ class AuthorizeUser
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user(); // ambil data user yg login
-                                  // fungsi user() diambil dari UserModel.php
-        if($user->hasRole($role)){ // cek apakah user punya role yg diinginkan
-            return $next($request);
+        $user_role = $request->user()->getRole();  // Ambil data level_kode dari user yang login
+
+        if (in_array($user_role, $roles)) { // Cek apakah level_kode user ada di dalam array roles
+            return $next($request); // Jika ada, maka lanjutkan request
         }
 
-        // jika tidak punya role, maka tampilkan error 403
+        // Jika tidak punya role, maka tampilkan error 403
         abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
 }
